e1c5a1ddd2d98b625ca41c7e01be95dfc1ccec41
snguyen171 <sonngn4@gmail.com>
2016-09-06 16:11:14 -0700

    Added a toast to speakButton. Added text to the pirate image.

--- app/src/main/java/edu/orangecoastcollege/cs273/snguyen171/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/snguyen171/yarrworld/MainActivity.java
@@ -1,14 +1,17 @@
 package edu.orangecoastcollege.cs273.snguyen171.yarrworld;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // Define the button as an instance variable (member variable)
-    Button speakButton;
-
+    private Button speakButton;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
         // Hook up the speakButton to the one in our view::
         //
         speakButton = (Button) findViewById(R.id.speakButton);
+
+        // Wire up the speakButton to perform an onClick event
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Use the static reference to "Toast" to display the message:
+                // Toast = vanishes after set amount of time
+                // Dialog = requires user input
+                Toast.makeText(context, R.string.button_text, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
